8785ae794bfab756d18e61286239283ae105fb41
Dmitry Safronov <dimasafonis@gmail.com>
2021-12-06 10:28:19 +0300

    Updated

--- app/src/main/java/dimasafonis/ecoapp/CameraActivity.kt
+++ app/src/main/java/dimasafonis/ecoapp/CameraActivity.kt
@@ -1,167 +1,124 @@
 package dimasafonis.ecoapp
 
 import android.Manifest.permission.CAMERA
-import android.content.Context
-import android.content.pm.PackageManager.PERMISSION_GRANTED
-import android.graphics.Bitmap
-import android.graphics.ImageFormat
+import android.content.Intent
+import android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION
+import android.content.Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+import android.content.pm.PackageManager.PERMISSION_DENIED
 import android.hardware.camera2.*
-import android.hardware.camera2.CameraDevice.TEMPLATE_PREVIEW
-import android.hardware.camera2.params.OutputConfiguration
-import android.hardware.camera2.params.SessionConfiguration
-import android.hardware.camera2.params.SessionConfiguration.SESSION_HIGH_SPEED
-import android.media.ImageReader
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.util.Log
-import android.view.Surface
-import android.view.TextureView
-import android.widget.ImageButton
-import androidx.annotation.RequiresPermission
-import androidx.appcompat.app.AlertDialog
-import com.googlecode.tesseract.android.TessBaseAPI
 import java.io.File
 
+import android.os.*
+import android.provider.MediaStore
+import android.provider.MediaStore.EXTRA_OUTPUT
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import com.google.android.material.button.MaterialButton
+import com.googlecode.tesseract.android.TessBaseAPI
+
+
 class CameraActivity : AppCompatActivity() {
-    private lateinit var preview: TextureView
-    private lateinit var cm: CameraManager // cameraManager
+//    private lateinit var dataFile: File
+//    private var isFirstTime = true
+//    private val data = object {
+//        var data: MutableMap<String, String> = mutableMapOf()
+//        fun save() {
+//            var text = ""
+//            data.forEach {
+//                val keyValue = arrayOf(it.key, it.value)
+//                text += keyValue.joinToString("=")
+//                text += ";"
+//            }
+//            text.removeSuffix(";")
+//            dataFile.writeBytes(text.toByteArray())
+//        }
+//        fun load() {
+//            data = mutableMapOf()
+//            if (dataFile.inputStream().available() == 0) return
+//            val pairs = dataFile.readText().split(";")
+//            pairs.forEach {
+//                val keyValue = it.split("=")
+//                data[keyValue[0]] = keyValue[1]
+//            }
+//        }
+//        operator fun get(key: String): String {
+//            return data[key] ?: ""
+//        }
+//        operator fun set(key: String, value: String) { data[key] = value }
+//    }
+
     private lateinit var tess: TessBaseAPI
     private lateinit var tesseractPath: File
-    private var cameras = arrayListOf<Camera>()
-    private var currentCamera: Int = 0
+    private lateinit var tmp: File
+    private val tag = "DimasafonisCodes"
+    private val cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == RESULT_OK) capture()
+    }
+    private val cameraPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        if (!it) {
+            Toast.makeText(this, "", Toast.LENGTH_LONG)
+            finish()
+        }
+    }
+
+    private var imageFile: File? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+        tmp = File(externalCacheDir!!, "tmp")
+        if (!tmp.exists()) tmp.mkdir()
+        tesseractPath = File(filesDir!!, "tesseract")
+        if(!tesseractPath.exists()) tesseractPath.mkdir()
 
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (checkSelfPermission(CAMERA) != PERMISSION_GRANTED)
-                requestPermissions(arrayOf(CAMERA), 1)
-        }
+        if (Build.VERSION.SDK_INT >= 23)
+            if (checkSelfPermission(CAMERA) == PERMISSION_DENIED) cameraPermission.launch(CAMERA)
 
-        cm = getSystemService(Context.CAMERA_SERVICE) as CameraManager
-        cm.cameraIdList.forEach{ id ->
-            cameras.add(Camera(id, cm, this))
-        }
-        preview = findViewById(R.id.preview)
-        findViewById<ImageButton>(R.id.capture).setOnClickListener { onCaptureTouch() }
-        findViewById<ImageButton>(R.id.changeCamera).setOnClickListener {
-            if (cameras[currentCamera].isOpen()) cameras[currentCamera].closeCamera()
-            if (++currentCamera > cameras.size - 1) {
-                currentCamera = 0
-            }
-            cameras[currentCamera].openCamera()
-        }
+        findViewById<MaterialButton>(R.id.capture).setOnClickListener { onCaptureTouch() }
+    }
+
+    override fun onResume() {
+        super.onResume()
         tesseractDataInit()
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 1)
-            if (grantResults[0] != PERMISSION_GRANTED) {
-                AlertDialog.Builder(this)
-                    .setMessage(R.string.camera_required)
-                    .setTitle(R.string.camera_required_title)
-                    .create().show()
-                if (Build.VERSION.SDK_INT >= 23) requestPermissions(arrayOf(CAMERA), 1)
-            }
-    }
-
     private fun tesseractDataInit() {
-        tesseractPath = File(this.cacheDir.absolutePath, "tessdata")
-        if (!tesseractPath.exists()) tesseractPath.mkdir()
+        val tessdata = File(tesseractPath, "tessdata")
+        if (!tessdata.exists()) tessdata.mkdir()
+        if (!tessdata.isDirectory) {
+            tessdata.delete()
+            tessdata.mkdir()
+        }
 
-        val engFile = File(tesseractPath.absolutePath, "eng.traineddata")
+        val engFile = File(tessdata, "eng.traineddata")
         val eng = assets.open("tesseract/eng.traineddata").readBytes()
 
-        if (!engFile.exists()) {
+        if (!engFile.exists())
             engFile.createNewFile()
+        if (!engFile.inputStream().readBytes().contentEquals(eng))
             engFile.writeBytes(eng)
-        }
-        else {
-            if (!engFile.inputStream().readBytes().contentEquals(eng))
-                engFile.writeBytes(eng)
-        }
+
     }
 
-    fun onCaptureTouch() {
+    private fun onCaptureTouch() {
+        val camera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        imageFile = File.createTempFile("tesseract", ".jpg", tmp)
+        val uri = FileProvider.getUriForFile(this, "$packageName.provider", imageFile!!)
+        camera.putExtra(EXTRA_OUTPUT, uri)
+        cameraLauncher.launch(camera)
+    }
+
+    private fun capture() {
         tess = TessBaseAPI()
-        tess.init(cacheDir.absolutePath, "eng")
-        tess.setVariable("language_model_penalty_non_dict_word", "1")
-        tess.setImage(preview.bitmap)
+        tess.init(tesseractPath.absolutePath, "eng")
+        tess.setVariable("language_model_penalty_non_dict_word", "0")
+        tess.setImage(imageFile)
         val text = tess.utF8Text
         Log.i("DimasafonisCodes", text)
-        tess.end()
-    }
-
-    class Camera(var id: String, var cm: CameraManager, val activity: CameraActivity) {
-
-        var cameraDevice: CameraDevice? = null
-        lateinit var captureSession: CameraCaptureSession
-        val callback = CameraCallback(this)
-        lateinit var imageReader: ImageReader
-
-        fun isOpen() : Boolean { return cameraDevice != null }
-
-        @RequiresPermission(value = CAMERA)
-        fun openCamera() {
-            cm.openCamera(id, callback, null)
-            startPreview()
-        }
-
-        fun closeCamera() {
-            if (cameraDevice != null) {
-                cameraDevice?.close()
-                cameraDevice = null
-            }
-        }
-
-        fun startPreview() {
-            activity.preview.surfaceTexture?.setDefaultBufferSize(1920, 1080)
-            val surface = Surface(activity.preview.surfaceTexture)
-            val builder = cameraDevice?.createCaptureRequest(TEMPLATE_PREVIEW)
-            builder?.addTarget(surface)
-            @Suppress("deprecation")
-            if (Build.VERSION.SDK_INT >= 28) {
-                cameraDevice?.createCaptureSession(SessionConfiguration(
-                    SESSION_HIGH_SPEED,
-                    arrayListOf(OutputConfiguration(surface)),
-                    { it.run() },
-                    CaptureCallback(this, builder as CaptureRequest.Builder)))
-            } else {
-                cameraDevice?.createCaptureSession(
-                    arrayOf(surface).toMutableList(),
-                    CaptureCallback(this, builder as CaptureRequest.Builder),
-                    null
-                )
-            }
-        }
-
-        class CaptureCallback(val camera: Camera, val builder: CaptureRequest.Builder) : CameraCaptureSession.StateCallback() {
-            override fun onConfigured(session: CameraCaptureSession) {
-                camera.captureSession = session
-                camera.captureSession.setRepeatingRequest(builder.build(), null, null)
-            }
-            override fun onConfigureFailed(session: CameraCaptureSession) {}
-        }
-
-        class CameraCallback(val camera: Camera) : CameraDevice.StateCallback() {
-            override fun onOpened(device: CameraDevice) {
-                camera.cameraDevice = device
-                camera.startPreview()
-                Log.i(null, "Camera ${device.id} opened")
-            }
-
-            override fun onDisconnected(device: CameraDevice) {
-                device.close()
-                camera.cameraDevice = null
-            }
-
-            override fun onError(device: CameraDevice, error: Int) {
-                Log.e("DimasafonisCodes", "Error with ${device.id}: $error")
-            }
-
-        }
+        tess.stop()
+        imageFile?.delete()
     }
 }
